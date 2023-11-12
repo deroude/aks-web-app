f063bb349932141b0a023f27405bc7b8bafce7eb
Valentin Raduti <raduti@adobe.com>
2023-11-12 22:56:22 +0200

    fixed terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,14 +65,14 @@ resource "azurerm_subnet" "aks" {
   name                 = "aks-subnet"
   resource_group_name  = "${azurerm_resource_group.rg.name}"
   virtual_network_name = "${azurerm_virtual_network.apim-aks.name}"
-  address_prefixes       = "10.10.1.0/24"
+  address_prefixes       = ["10.10.1.0/24"]
 }
 
 resource "azurerm_subnet" "apim" {
   name                 = "apim-subnet"
   resource_group_name  = "${azurerm_resource_group.rg.name}"
   virtual_network_name = "${azurerm_virtual_network.apim-aks.name}"
-  address_prefixes       = "10.10.2.0/24"
+  address_prefixes       = ["10.10.2.0/24"]
 }
 
 // create AKS cluster
